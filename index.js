fcb4eac6343812ff216887dd61ef3bec43997be4
jooj4014-wq <jooj4014@gmail.com>
2026-01-04 09:24:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ const http = require("http");
 
 const PORT = process.env.PORT || 3000;
 
-http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Server is running ✅");
-}).listen(PORT, () => {
-  console.log("Server running on port", PORT);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Server is running 🚀");
+  }
+
+  else if (req.url === "/api/test") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({
+      status: "success",
+      message: "API is working ✅"
+    }));
+  }
+
+  else {
+    res.writeHead(404);
+    res.end("Not Found");
+  }
 });
+
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});;
